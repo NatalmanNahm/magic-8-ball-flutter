ec29e5dff8cea5897b4129075bf59bee8a859a1e
NatalmanNahm <natalnahm@gmail.com>
2021-06-16 17:18:16 -0500

    Made the image change on a press of a button using the math library.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -26,12 +27,21 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Image.asset('images/ball1.png'),
-      ),
+          child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print(ballNumber);
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      )),
     );
   }
 }
